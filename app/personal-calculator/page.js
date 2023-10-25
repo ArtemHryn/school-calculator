17ddfe79f149e836c0c2830da9037ebbb42d02e4
Artem <grynbest@gmail.com>
2023-10-25 22:06:37 +0300

    added metadata

--- app/personal-calculator/page.js
+++ app/personal-calculator/page.js
@@ -2,12 +2,29 @@ import Container from "@components/Container/Container";
 import Hero from "@components/Hero/Hero";
 import PersonalCalculator from "@components/PersonalCalculator/PersonalCalculator";
 
-export const metadata = {
-  title: "Персональний калькулятор",
-  description: "Калькулятор для розрахунку персональной матриці",
-  viewport:
-    "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
-};
+export async function generateMetadata() {
+  return {
+    metadataBase: new URL("https://karmolog4u.vercel.app"),
+    title: "Персональний калькулятор",
+    viewport:
+      "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+    description: "Калькулятор для розрахунку персональной матриці",
+
+    alternates: {
+      canonical: "/",
+      languages: {
+        "uk-UA": `/`,
+      },
+    },
+    openGraph: {
+      title: "Персональний калькулятор",
+      viewport:
+        "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+      description: "Калькулятор для розрахунку персональной матриці",
+      url: "/",
+    },
+  };
+}
 
 const PersonalCalculatorPage = () => {
   return (
